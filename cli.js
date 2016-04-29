6ce80644906a8bcbd34868049a14c18be9aea3b7
Jordan Last <jordan.michael.last@gmail.com>
2016-04-28 22:49:08 -0600

    possible command line could work now

--- cli.js
+++ cli.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 var exec = require('child_process').exec;
-var cmd = 'npm start';
+var cmd = 'node_modules/.bin/electron-spawn main.js index.html';
 
 var newProcess = exec(cmd);
 
 newProcess.stdout.on('data', function(data) {
+    console.log(data);
+});
+
+newProcess.stderr.on('data', function(data) {
     console.log(data);
 });
