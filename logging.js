72cc3e1e53f0a906878666c0ef6d5c7cb9fbf61b
Jordan Last <jordan.michael.last@gmail.com>
2016-05-03 00:44:01 -0600

    file paths are working

--- logging.js
+++ logging.js
@@ -1,6 +1,6 @@
 const ipc = electron.ipcMain;
 
 //TODO get this to work for all console methods and any number of parameters
-window.console.log = (...theArgs) => {
+console.log = (...theArgs) => {
     ipc.send('asynchronous-message', ...theArgs);
 };
--- main.js
+++ main.js
@@ -5,20 +5,16 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const ipc = electron.ipcMain;
 
-//const filename = process.argv[2] || 'index.html';
-//const filePath = __dirname + '/' + filename;
-//const filePath = __dirname.split('/').slice(0, -2).join('/') + '/' + filename;
-
-//console.log(filePath);
+const filename = process.argv[2] || 'index.html';
+const filepath = __dirname + '/' + filename; //development filepath from the scram-engine repo
+//const filepath = __dirname.split('/').slice(0, -2).join('/') + '/' + filename; //production filepath from the consumer repo (assumes first level of node_modules directory from root of repo)
 
 let mainWindow = null;
 
 app.on('ready', () => {
         mainWindow = new BrowserWindow({show:false});
         console.log('before call to loadURL');
-        //mainWindow.loadURL(`file://${__dirname}/${filename}`);
-        mainWindow.loadURL(`file://${__dirname}/index.html`);
-        //console.log(`file://${__dirname}/${filename}`);
+        mainWindow.loadURL(`file://${filepath}`);
         console.log('after call to loadURL');
 });
 
