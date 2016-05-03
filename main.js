b71c322515fab6e74a19e3f341d151d28c0517a8
Jordan Last <jordan.michael.last@gmail.com>
2016-05-03 01:02:04 -0600

    adding some todos

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const BrowserWindow = electron.BrowserWindow;
 const ipcMain = electron.ipcMain;
 
 const filename = process.argv[2] || 'index.html';
+//TODO figure out a more generally applicable way of getting the filepath. Also, eventually it might be nice if the user could do an npm script like this: scram-engine index.html
 //const filepath = __dirname + '/' + filename; //development filepath from the scram-engine repo
 const filepath = __dirname.split('/').slice(0, -2).join('/') + '/' + filename; //production filepath from the consumer repo (assumes first level of node_modules directory from root of repo)
 
@@ -13,9 +14,7 @@ let mainWindow = null;
 
 app.on('ready', () => {
         mainWindow = new BrowserWindow({show:false});
-        console.log('before call to loadURL');
         mainWindow.loadURL(`file://${filepath}`);
-        console.log('after call to loadURL');
 });
 
 ipcMain.on('asynchronous-message', (e, ...args) => {
