b9158321674d764222666f7ada94c15ca24d14e2
Jordan Last <jordan.michael.last@gmail.com>
2016-05-04 20:49:50 -0600

    adding dev mode

--- main.js
+++ main.js
@@ -5,16 +5,21 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const ipcMain = electron.ipcMain;
 
-const filename = process.argv[2] || 'index.html';
-//TODO figure out a more generally applicable way of getting the filepath. Also, eventually it might be nice if the user could do an npm script like this: scram-engine index.html
-//const filepath = __dirname + '/' + filename; //development filepath from the scram-engine repo
-const filepath = __dirname.split('/').slice(0, -2).join('/') + '/' + filename; //production filepath from the consumer repo (assumes first level of node_modules directory from root of repo)
+const filename = process.argv[2];
+const devMode = process.argv[3] === '-d' ? true : false;
+const filepath = __dirname.split('/').slice(0, -2).join('/') + '/' + filename;
 
 let mainWindow = null;
 
 app.on('ready', () => {
-        mainWindow = new BrowserWindow({show:false});
+        mainWindow = new BrowserWindow({
+            show: devMode
+        });
         mainWindow.loadURL(`file://${filepath}`);
+
+        if (devMode) {
+            mainWindow.webContents.openDevTools();
+        }
 });
 
 ipcMain.on('asynchronous-message', (e, ...args) => {
