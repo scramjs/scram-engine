295f24ce4209a0bd98ea9950d2f30db91ccb44ef
Jordan Last <jordan.michael.last@gmail.com>
2016-05-28 12:21:21 -0600

    loading from file url works

--- main.js
+++ main.js
@@ -6,13 +6,31 @@ const BrowserWindow = electron.BrowserWindow;
 const ipcMain = electron.ipcMain;
 
 const argOptions = process.argv.slice(3);
-
 const filename = process.argv[2];
-const devMode = argOptions.includes('-d') ? true : false;
+const devMode = argOptions.includes('-d');
+const loadFromFile = argOptions.includes('-f');
 
-const protocol = 'http';
-const domain = 'localhost';
-const port = ':5001';
+const getIndexURL = (loadFromFile, filename) => {
+
+    const createFileURL = (filename) => {
+        return `file://${__dirname}/${filename}`;
+    };
+
+    const createServerURL = (filename) => {
+        const protocol = 'http';
+        const domain = 'localhost';
+        const port = ':5001/';
+
+        return `${protocol}://${domain}${port}${filename}`;
+    };
+
+    if (loadFromFile) {
+        return createFileURL(filename);
+    }
+    else {
+        return createServerURL(filename);
+    }
+};
 
 const startLocalServer = () => {
     require('child_process').exec(`node_modules/.bin/http-server -p 5001`, (err, stdout, stderr) => {
@@ -25,7 +43,7 @@ const startLocalServer = () => {
     });
 };
 
-const launchApp = () => {
+const launchApp = (indexURL) => {
     let mainWindow = null;
 
     app.on('ready', () => {
@@ -33,7 +51,7 @@ const launchApp = () => {
                 show: devMode
             });
 
-            mainWindow.loadURL(`${protocol}://${domain}${port}/${filename}`);
+            mainWindow.loadURL(indexURL);
 
             if (devMode) {
                 mainWindow.webContents.openDevTools();
@@ -45,5 +63,11 @@ const launchApp = () => {
     });
 };
 
-startLocalServer();
-launchApp();
+const init = () => {
+    if (!loadFromFile) {
+        startLocalServer();
+    }
+    launchApp(getIndexURL(loadFromFile, filename));
+};
+
+init();
