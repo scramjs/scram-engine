422fa4f54b80ec2078380626628c39155e5d57ff
Jordan Last <jordan.michael.last@gmail.com>
2016-07-08 10:02:31 -0600

    testing

--- filesystem-config.js
+++ filesystem-config.js
@@ -4,3 +4,5 @@ const filename = require('electron').remote.getCurrentWindow().getFilename();
 
 __dirname = scramPathModule.resolve(__dirname, '../../../../../../', filename.split('/').slice(0, -1).join('/')); //TODO this path is possibly subject to change if Electron changes its file structure
 __filename = scramPathModule.resolve(__dirname, '../../../../../../', filename); //TODO this path is possibly subject to change if Electron changes its file structure
+
+console.log('hello')
